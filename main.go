f022b3b5e4030ed1dcaf24c809e0596a3a02f2dc
xenkidu <aburton85@live.com>
2021-12-06 18:15:56 -0700

    Hello Gorilla.

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func helloGoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello net/http!\n"))
+	w.Write([]byte("Hello Gorilla!\n"))
 }
 
 func main() {
-	http.HandleFunc("/", helloGoHandler)
-	log.Fatal(http.ListenAndServe(":8181", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloGoHandler)
+	log.Fatal(http.ListenAndServe(":8181", router))
 }
